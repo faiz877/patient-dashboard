60cd1bc9abccf1af3109e719bc7272bd4aa1f59b
Faiz Khan <Faiz.Khan@tpv-tech.com>
2025-05-14 00:31:20 +0530

    chore : added deployed url

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -15,7 +15,7 @@ const Dashboard: FC = () => {
       setError('');
       try {
         const token = localStorage.getItem('token');
-        const res = await fetch('http://localhost:5000/api/auth/me', {
+        const res = await fetch('https://patient-dashboard-6cac.onrender.com/api/auth/me', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
